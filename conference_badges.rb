b2dc3a0814260a06a4b4468b04e81d4d3c2e0845
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 19:03:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each {|name| "Hello, my name is #{name}."}
+  batch_badges = []
+  attendees.each {|name| batch_badges << "Hello, my name is #{name}."}
 end
